fc8edaffc4220794a0c927491f13b1ab733c2b2d
ChristopheMalo <christof.malo@gmail.com>
2015-10-09 12:50:23 +0200

    Déclarer des variables

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Comment dynamiser un site avec Javascript
 
 ## Les bases du Javascript
 - Affichage d'une boite de dialogue
+- Fichier JS externe
+- Les variables
---
+++ public_html/javascript/code_js_02.js
@@ -0,0 +1,16 @@
+/* 
+ * Déclaration des variables
+ */
+var uneVariable;
+var maVariable01, maVariable02, maVariable05 = 2;
+var maVariable03 = 'Texte';
+var maVariable04 = 1.5;
+var maVariable06 = true, maVariable07 = false;
+
+/*
+ * Vérifier le type de la variable
+ */
+alert(typeof maVariable03 + ' - ' + typeof uneVariable);
+alert(typeof uneVariable);
+alert(typeof maVariable03);
+alert(typeof maVariable06);
