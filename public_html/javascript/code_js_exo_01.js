fbf5ba83089b37b8ae10ac6fbb2e79a169b0ba76
ChristopheMalo <christof.malo@gmail.com>
2015-10-09 13:51:45 +0200

    Coder exercice 01 Dis bonjour

---
+++ public_html/javascript/code_js_exo_01.js
@@ -0,0 +1,7 @@
+/* 
+ * Code Javascript exercice 01 - Dis bonjour
+ * à ce stade aucun contrôle n'est effectué sur la saisie de l'internaute
+ */
+var userFirstname = prompt('Entre ton prénom :');
+
+alert('Bonjour ' + userFirstname);
