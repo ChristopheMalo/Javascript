99f7a37bf07ad4698c39ba4c7bda28d05847a205
ChristopheMalo <christof.malo@gmail.com>
2015-11-11 21:00:38 +0100

    Coder exercice 11 Mateo et la canicule

---
+++ public_html/javascript/code_js_exo_11.js
@@ -0,0 +1,20 @@
+/* 
+ * Code Javascript exercice 11 - Mateo et la canicule
+ * Pas de contrôle de saisie de l'utilisateur
+ * 
+ */
+// Initialiser le tableau d'origine
+var prenoms = ['Pierre'];
+
+// La boucle demande 2 prénoms
+while (prenoms.length < 3) {
+    prenom = prompt('Ajoute un prénom à la liste : ');
+    prenoms.push(prenom);
+}
+
+// console.log(prenoms); // Debug
+
+// Parcourir le tableau et afficher les 3 prénoms
+for (var person in prenoms) {
+    console.log(prenoms[person]);
+}
