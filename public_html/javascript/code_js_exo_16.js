f63223ef8b4551a19778b3cf9d3ac79c40b8fd3c
ChristopheMalo <christof.malo@gmail.com>
2015-11-17 19:58:11 +0100

    Coder exercice 16 Insérer des éléments li dans une liste ol

---
+++ public_html/javascript/code_js_exo_16.js
@@ -0,0 +1,27 @@
+/* 
+ * Code Javascript exercice 16 - Insérer des éléments dans une liste
+ * 
+ */
+// Récupérer nom de OL pour poistionner les li
+var ol = document.getElementById('name_ol_id');
+
+// Création des textes
+var textesLi = [
+    document.createTextNode('Élement li insertion position after 5'),
+    document.createTextNode('Élement li insertion last position')
+];
+
+// Création des éléments
+var li1 = document.createElement('li');
+var li2 = document.createElement('li');
+
+// Insertion des textes dans les éléments
+li1.appendChild(textesLi[0]);
+li2.appendChild(textesLi[1]);
+
+// Récupérer position li
+var liPosition = ol.getElementsByTagName('li');
+
+// Insertion des éléments dans le 'conteneur' OL
+ol.insertBefore(li1, liPosition[5]);
+ol.appendChild(li2);
