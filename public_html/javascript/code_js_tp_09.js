613b11a9099f448570abfa18ea47f33c1cb95cd9
ChristopheMalo <christof.malo@gmail.com>
2015-11-23 19:12:21 +0100

    Coder TP 09 Formulaire intéractif

---
+++ public_html/javascript/code_js_tp_09.js
@@ -0,0 +1,32 @@
+/* 
+ * Code Javascript TP 09 - Un formulaire interactif
+ * 
+ * 
+ */
+// Récupérer tous les id des éléments du formulaire
+var dataPrenom          = document.getElementById('prenom'),
+    dataNom             = document.getElementById('nom'),
+    dataAge             = document.getElementById('age'),
+    dataPseudo          = document.getElementById('pseudo'),
+    dataPassword        = document.getElementById('password'),
+    dataConfirmPassword = document.getElementById('confirmpassword'),
+    dataCountry         = document.getElementById('country');
+        
+
+// Insérer le data-content de chaque élément du formulaire
+dataPrenom.setAttribute('data-content', 'Ton prénom ne peut pas faire moins de 2 caractères');
+dataNom.setAttribute('data-content', 'Ton nom ne peut pas faire moins de 2 caractères');
+dataAge.setAttribute('data-content', 'Ton âge doit être compris entre 5 et 140 ans');
+dataPseudo.setAttribute('data-content', 'Le pseudo ne peut pas faire moins de 4 caractères');
+//dataPassword.setAttribute('Le mot de passe ne doit pas faire moins de 6 caractères');
+//dataConfirmPassword.setAttribute('Le mot de passe de confirmation doit être identique à celui d\'origine');
+
+// Créer la dynamiquement la création des popover informations des champs du formulaire
+$(document).ready(function() {
+   $('input').popover({
+       container: 'form', // Spécifier le container global -> permet au popover de prendre une largeur plus importante
+       toggle: 'popover',
+       placement: 'top',
+       trigger  : 'hover'
+   });
+});
