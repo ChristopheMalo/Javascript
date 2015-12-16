96ff5b53f60b49f42949f3ef5b7b29f7384557d8
ChristopheMalo <christof.malo@gmail.com>
2015-12-16 21:10:25 +0100

    Modifier calcul total déplacement exercice 25

--- public_html/javascript/code_js_exo_25.js
+++ public_html/javascript/code_js_exo_25.js
@@ -9,32 +9,49 @@ tableauPoints = [
     [2,8]
 ];
 
+
 // Fonction de Calcul de déplacement entre 2 points
 // Arguments coordonnées (x,y) de 2 points (1,2)
 function calculDeplacement2points (x1, y1, x2, y2) {
+    
     var result = Math.abs(x1 - x2) + Math.abs(y1 - y2);
     return result;
+    
 }
 
 
-// Fonction de calcul de distance
+// Fonction de calcul de déplacement
 // Argument : un tableau multipoints
 function calculDeplacement (tableau) {
-    // Initialiser variable du résultat
-    var deplacement;
+    
+    // Initialiser variables
+    var deplacement,    // Variable pour calculer le deplacement
+        total = [];     // Array pour comptabiliser les déplacement
     
     // Parcourir le tableau (array) en argument
     for (var i = 0; i < tableau.length - 1; i++) {
         
         // Appel de la fonction de calcul de distance entre 2 points
         deplacement = calculDeplacement2points(tableau[i][0], tableau[i][1], tableau[i+1][0], tableau[i+1][1]);
-        deplacement++;
+        total.push(deplacement); // Ajouter les déplacements dans array total
+       
+    }
+    
+    
+    var result = 0; // Initialisation de la variable de résultat du calcul des déplacements
+    
+    // Parcourir arra ytotal pour calcul total des déplacements
+    for (var i = 0; i < total.length; i++){
+        
+        result += total[i]; // Calcul
     
     }
     
-    return deplacement;
+    
+    return result; // Retourne le résultat
 
-    }
+}
+
 
 // Appel de la fonction pour afficher le deplacement entre les points
 console.log('La distance est : ' + calculDeplacement(tableauPoints)); // Retourne 4
