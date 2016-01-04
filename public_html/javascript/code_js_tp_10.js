360a19b60fffd6339f61045de3ce511102d44173
ChristopheMalo <christof.malo@gmail.com>
2016-01-04 19:45:27 +0100

    Coder un second constructeur pour les caracteristiques du robot tp robot POO

--- public_html/javascript/code_js_tp_10.js
+++ public_html/javascript/code_js_tp_10.js
@@ -10,23 +10,43 @@ var Robot = {
 //    vm  : 3,      // Vitesse maximale
 //    pos : [0,0],    // Position d'origine
      
-    // Initialisation du robot
+    // Initialisation du nom du rot du robot
     init: function (nom) {
         this.nom    = nom;      // Nom du personnage
         
-        // OU Caractéristiques identiques pour tous les robots
-        // Préference d'intiialisation de toutes les propriétés dans la fonction d'initialisation
-        this.pdv    = 100;      // Point de vie initial
-        this.vm     = 3;        // Vitesse maximale
-        this.pos    = [0,0];    // Position d'origine
     },
     
-    // Description du robot
+    // 2ème constructeur pour initialiser les caracteristiques du robot
+    // Ce constructeur peut-être rassembler dans le init
+    initCaracteristiques: function (pdv, vm, coordX, coordY) {
+        this.pdv    = pdv;      // Point de vie
+        this.vm     = vm;       // Vitesse maximale
+        
+        this.coordX = coordX;   // Position x
+        this.coordY = coordY;   // Position y
+        // this.pos    = [coordX,coordY];    // Position d'origine sous forme d'array
+        // ou this.pos = [this.x, this.y];
+    },
+    
+    // Fonstion pour récupérer les coordonnées du robot
+    arrayCoordonnees: function () {
+        return this.pos = [this.coordX, this.coordY];
+    },
+    
+    // Présentation du robot
     sePresenter: function () {
+        var optionCase = '';
+            
+        if (this.vm === 1) {
+            optionCase = 'case';
+        } else {
+            optionCase = 'cases';
+        }
+        
         var description = 'Bonjour, je suis un robot et je m\'appelle ' + this.nom
                         + '. J\'ai '  + this.pdv + ' points de vie.'
-                        + ' Ma vitesse de déplacement est de ' + this.vm + ' cases par seconde.'
-                        + ' Ma position actuelle est à la case de coordonnées (' + this.pos + ').';
+                        + ' Ma vitesse de déplacement est de ' + this.vm + ' ' + optionCase + ' par seconde.'
+                        + ' Ma position actuelle est à la case de coordonnées (' + this.arrayCoordonnees() + ').'; // ou à la place de this.pos --> this.arrayCoordonnees()
         return  description;
     }
     
@@ -35,6 +55,7 @@ var Robot = {
 // Création d'un robot - R2D2
 var r2d2 = Object.create(Robot);
 r2d2.init('R2D2');
+r2d2.initCaracteristiques(100, 3, 0,0);
 
 // Gestion affichage un robot
 console.log(r2d2.sePresenter());
@@ -42,13 +63,16 @@ console.log(r2d2.sePresenter());
 // Création d'un robot Terminator
 var terminator = Object.create(Robot);
 terminator.init('Terminator');
+terminator.initCaracteristiques(50, 1, 8, 7);
 console.log(terminator.sePresenter());
 
 var androidMan = Object.create(Robot);
-androidMan.init('Android Man');
+androidMan.init('Android Man', 10, 10);
+androidMan.initCaracteristiques(80, 2, 10, 10);
 
 var lukeSkyWalker = Object.create(Robot);
-lukeSkyWalker.init('Luke Sky-Walker');
+lukeSkyWalker.init('Luke Sky-Walker', 7, 7);
+lukeSkyWalker.initCaracteristiques(90, 3, 7, 7);
 
 // Création d'un array (Tableau) + Ajouter les robots créés au tableau
 var armyRobots = [r2d2, terminator, androidMan, lukeSkyWalker];
